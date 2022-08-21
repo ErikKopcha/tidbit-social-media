c330836155e1488c06a06aebebab7e429ba5ed93
EK <erickopcha@gmail.com>
2022-08-21 16:35:43 +0300

    update: types, config, styles

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['cdn.vox-cdn.com']
+  },
   swcMinify: true,
   compiler: {
     // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
